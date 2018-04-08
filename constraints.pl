6b500b39259f741425110ec876aae5b34045f8bc
benstrac <35542776+benstrac@users.noreply.github.com>
2018-04-07 21:40:08 -0600

    added documentation to constraints

--- constraints.pl
+++ constraints.pl
@@ -1,3 +1,4 @@
+    %Example asserts in the db
 fpa(0,'X').
 fpa(1,'A').
 %fpa(2,'B').
@@ -24,7 +25,8 @@ tnp('X','Y',0).
 tnp('E','F',17).
    
     
-    
+    %checkFPA(Task List, Current Machine)
+    % first call is with current machine 1
 checkFPA([],_).
 checkFPA([H|T],Mach) :- fpa(Mach,Z) -> H==Z, 
     NewMach is Mach + 1, 
@@ -32,6 +34,8 @@ checkFPA([H|T],Mach) :- fpa(Mach,Z) -> H==Z,
     NewMach is Mach + 1, 
     checkFPA(T,NewMach).
 
+    %checkFM(Task List, Current Machine)
+    % first call is with current machine 1
 checkFM([],_).
 checkFM([H|T],Mach) :- fm(Mach,Z) -> H\=Z, 
     NewMach is Mach + 1, 
@@ -39,25 +43,37 @@ checkFM([H|T],Mach) :- fm(Mach,Z) -> H\=Z,
     NewMach is Mach + 1,
     checkFM(T,NewMach).
 
-
+    %checkTNT(Task List, Task on Machine 1)
+    % first call has to send the head of the list in order to check wrap around
 checkTNT([],_).
 checkTNT([Last|[]],Head) :- tnt(Last,Z) -> Head\=Z, !; 
     checkTNT([],_).
 checkTNT([H|[N|T]],Head) :- tnt(H,N) -> false, !; 
     checkTNT(T,Head).
 
+    %checkHARD(Task List)
+    % simply send the task list
+    % will return true or false
 checkHARD([H|T]) :- checkFPA([H|T],1), 
     checkFM([H|T],1), 
     checkTNT([H|T],H).
       
 
+    %getMP(Current Machine, Task List, Penalty)
+    % total penalty value is returned through the third parameter
+    
+    % getting instantiation error
 getMP(_,[],_).
 getMP(Mach,[Task|T],Total) :- mp(Mach,Task,Z) -> NewTotal is Total + Z, 
     NewMach is Mach + 1,
     getMP(NewMach,T,NewTotal), !; 
     NewMach is Mach + 1, 
     getMP(NewMach, T, Total).
-    
+
+    %getTNP(Currrent Machine, Task List, Penalty)
+    % total penalty value is returned through the third parameter
+
+    % getting instantiation error
 getTNP(_,[],_).
 getTNP(Head,[Last|[]],Total) :- tnp(Last,Head,Z) -> NewTotal is Total + Z, 
     getTNP(_,[],NewTotal), !;
@@ -68,6 +84,8 @@ getTNP(Head,[T1|[T2|T]],Total) :- tnp(T1,T2,Z) -> NewTotal is Total + Z,
     NewTotal is Total + Z,
     getTNP(Head,[T2|T],NewTotal).
 
+    %getSOFT(Task List, Penalty Total)
+    % total penalty value is returned through the second parameter
 getSOFT([H|T],Total) :- getMP(1,[H|T],MPtotal), 
     getTNP(H,[H|T],TNPtotal), 
     Total is MPtotal + TNPtotal.
