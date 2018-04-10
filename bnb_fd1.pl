7e907acd6d9dd6a7232b66fdfe73745b553e5b3e
Anastasiya Lazarenko <AnaScarlet@users.noreply.github.com>
2018-04-09 23:37:21 -0600

    BNB Works!?
    Tested on the given constraints

---
+++ bnb_fd1.pl
@@ -0,0 +1,21 @@
+:- [constraints].
+
+% Only redoes soft constraints
+ bnb(S) :- fd_minimize(constraints(S,P), P) , !.
+
+ constraints(S,P) :- 
+    S = [_1,_2,_3,_4,_5,_6,_7,_8], 
+    fd_domain(S, 65, 72) , 
+    checkFPA(S,1) ,
+    fd_all_different(S) ,
+    fd_labeling(S) ,
+    hard_constr(S) ,
+    getSOFT(S, P) , 
+    fd_labeling(P).
+
+
+% T parameter has to receive a list of variables for this to make sense
+% values of predicates are values of ASCII characters
+ hard_constr([H|T]) :- 
+    checkFM([H|T],1) -> checkTNT([H|T],H).
+
--- constraints.pl
+++ constraints.pl
@@ -1,36 +1,9 @@
-    %Example asserts in the db
-fpa(0,'X').
-fpa(1,'A').
-%fpa(2,'B').
-fpa(3,'C').
-fpa(4,'D').
-fpa(5,'E').
-%fpa(6,'F').
-%fpa(7,'G').
-%fpa(8,'H').
-
-fm(0,'X').
-fm(6,'F').
-
-tnt('X','Y').
-tnt('A','B').
- 
-mp(0,'X',0).
-mp(1,'A',10).
-mp(2,'B',11).
-mp(3,'C',12).
-mp(4,'D',13).
-
-tnp('X','Y',0).
-%tnp('E','F',17).
-tnp('F','G',3).
-tnp('H','A',1).
-   
+:- [rules_machpen2]. 
     
     %checkFPA(Task List, Current Machine)
     % first call is with current machine 1
 checkFPA([],_).
-checkFPA([H|T],Mach) :- fpa(Mach,Z) -> H==Z, 
+checkFPA([H|T],Mach) :- fpa(Mach,H) , 
     NewMach is Mach + 1, 
     checkFPA(T,NewMach), !; 
     NewMach is Mach + 1, 
@@ -39,26 +12,22 @@ checkFPA([H|T],Mach) :- fpa(Mach,Z) -> H==Z,
     %checkFM(Task List, Current Machine)
     % first call is with current machine 1
 checkFM([],_).
-checkFM([H|T],Mach) :- fm(Mach,Z) -> H\=Z, 
+checkFM([H|T],Mach) :- \+ fm(Mach,H), 
     NewMach is Mach + 1, 
-    checkFM(T,NewMach), !; 
-    NewMach is Mach + 1,
     checkFM(T,NewMach).
 
     %checkTNT(Task List, Task on Machine 1)
     % first call has to send the head of the list in order to check wrap around
 checkTNT([],_).
-checkTNT([Last|[]],Head) :- tnt(Last,Z) -> Head\=Z, !; 
+checkTNT([Last|[]],Head) :- \+ tnt(Last,Head), 
     checkTNT([],_).
-checkTNT([H|[N|T]],Head) :- tnt(H,N) -> false, !; 
+checkTNT([H|[N|T]],Head) :- \+ tnt(H,N), 
     checkTNT(T,Head).
 
     %checkHARD(Task List)
     % simply send the task list
     % will return true or false
-checkHARD([H|T]) :- checkFPA([H|T],1), 
-    checkFM([H|T],1), 
-    checkTNT([H|T],H).
+checkHARD([H|T]) :- checkFPA([H|T],1) -> checkFM([H|T],1) -> checkTNT([H|T],H).
       
 
     %getMP(Current Machine, Task List, Penalty)
@@ -73,10 +42,10 @@ getMP(Mach,[Task|T],Total) :- mp(Mach,Task,Z) -> NewMach is Mach + 1,
     %getTNP(Currrent Machine, Task List, Penalty)
     % total penalty value is returned through the third parameter
 getTNP(_,[],0).
-getTNP(Head,[Last|[]],Total) :- tnp(Last,Head,Z) -> Total is Z, !;
+getTNP(Head,[Last|[]],Total) :- tnp(Last,Head,Z) -> Total is Z ; !,
     Total is 0, !.
 getTNP(Head,[T1|[T2|T]],Total) :- tnp(T1,T2,Z) -> getTNP(Head,[T2|T],NewTotal),
-    Total is NewTotal + Z, !; 
+    Total is NewTotal + Z, !;
     getTNP(Head,[T2|T],NewTotal),
     Total is NewTotal.
 
