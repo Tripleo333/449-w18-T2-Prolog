9c2fac6ee166deb1cd5d8dec5bb94492ce4fe080
Tripleo333 <Omieabdelwahab@gmail.com>
2018-04-13 01:52:20 -0600

    combining rules, parser, main, constraints, bnb

--- parser.pl
+++ parser.pl
@@ -377,3 +377,96 @@ getTriple(X,Y) :- nth(2, X, TONE), nth(4, X, TTWO), clearList(X, F), number_code
 % X = list of chars to clear
 % Y = cleared list
 clearList(X,Y) :- delete(X, 65, One), delete(One, 66, Two), delete(Two, 67, Three), delete(Three, 68, Four), delete(Four, 69, Five), delete(Five, 70, Six), delete(Six, 71, Seven), delete(Seven, 72, Eight), delete(Eight, 44, Nine), delete(Nine, 40, Ten), delete(Ten, 41, Eleven), delete(Eleven, 32, Twelve), delete(Twelve, 9, Thirteen), delete(Thirteen, 10, Y), !.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%Constraints%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %checkFPA(Task List, Current Machine)
+    % first call is with current machine 1
+checkFPA([],_).
+checkFPA([H|T],Mach) :- fpa(Mach,H) , 
+    NewMach is Mach + 1, 
+    checkFPA(T,NewMach), !; 
+    NewMach is Mach + 1, 
+    checkFPA(T,NewMach).
+
+    %checkFM(Task List, Current Machine)
+    % first call is with current machine 1
+checkFM([],_).
+checkFM([H|T],Mach) :- \+ fm(Mach,H), 
+    NewMach is Mach + 1, 
+    checkFM(T,NewMach).
+
+    %checkTNT(Task List, Task on Machine 1)
+    % first call has to send the head of the list in order to check wrap around
+checkTNT([],_).
+checkTNT([Last|[]],Head) :- \+ tnt(Last,Head), 
+    checkTNT([],_).
+checkTNT([H|[N|T]],Head) :- \+ tnt(H,N), 
+    checkTNT([N|T],Head).
+
+    %checkHARD(Task List)
+    % simply send the task list
+    % will return true or false
+% checkHARD([H|T]) :- checkFPA([H|T],1) -> checkFM([H|T],1) -> checkTNT([H|T],H).
+      
+
+    %getMP(Current Machine, Task List, Penalty)
+    % total penalty value is returned through the third parameter
+getMP(_,[],0).
+getMP(Mach,[Task|T],Total) :- mp(Mach,Task,Z) -> NewMach is Mach + 1,
+    getMP(NewMach,T,NewTotal),
+    Total is NewTotal + Z, !;
+    NewMach is Mach + 1,
+    getMP(NewMach, T, Total), !.
+
+    %getTNP(Currrent Machine, Task List, Penalty)
+    % total penalty value is returned through the third parameter
+getTNP(_,[],0).
+getTNP(Head,[Last|[]],Total) :- tnp(Last,Head,Z) -> Total is Z ; !,
+    Total is 0, !.
+getTNP(Head,[T1|[T2|T]],Total) :- tnp(T1,T2,Z) -> getTNP(Head,[T2|T],NewTotal),
+    Total is NewTotal + Z, !;
+    getTNP(Head,[T2|T],NewTotal),
+    Total is NewTotal.
+
+    %getSOFT(Task List, Penalty Total)
+    % total penalty value is returned through the second parameter
+getSOFT([H|T],Total) :- getMP(1,[H|T],MPtotal), 
+    getTNP(H,[H|T],TNPtotal), 
+    Total is MPtotal + TNPtotal.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%BNB%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Only redoes soft constraints
+ bnb(S,P) :- fd_minimize(constraints(S,P), P) , !.
+
+ constraints(S,P) :- 
+    S = [_1,_2,_3,_4,_5,_6,_7,_8], 
+    fd_domain(S, 65, 72) , 
+    checkFPA(S,1) ,
+    fd_all_different(S) ,
+    fd_labeling(S) ,
+    hard_constr(S) ,
+    getSOFT(S, P) , 
+    fd_labeling(P).
+
+
+% T parameter has to receive a list of variables for this to make sense
+% values of predicates are values of ASCII characters
+ hard_constr([H|T]) :- 
+    checkFM([H|T],1) -> checkTNT([H|T],H).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%Ben #\= Rules%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fpa(0,0).
+fm(0,0).
+tnt(0,0).
+mp(0,0,0).
+tnp(0,0,0).
+
